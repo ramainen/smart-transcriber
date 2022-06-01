1fb6444ca158e28639a6bbb9cbe91def91346cd5
Damir Fakhrutdinov <ainu.sky@gmail.com>
2022-06-01 09:06:30 +0300

    Update main.go

--- main.go
+++ main.go
@@ -139,13 +139,166 @@ func (obj *Transpiler) Transpile(text string) []string {
 
 	result = append(result, newWordEnglish)
 
+	/////////////////////
+
+	multilangRules := [][][]string{
+
+		////////////////////////////
+
+		//FRA
+
+		{
+			{`eaux\Z`, `о`}, {`beaut`, `бьют`}, {`eau`, `о`}, {`ogne\Z`, `он`},
+			{`gnie`, `йн`}, {`agne`, `ейн`}, {`ouge`, `уж`}, {`oix`, `уа`}, {`iei`, `ье`},
+			{`oux`, `о`}, {`(?<=\w)qu[eéè]\Z`, `к`}, {`ch`, `ш`},
+
+			{`\Ales\Z`, `ле`}, {`\Ac`, `к`},
+			{`\A[eéè](?![euioayéèàù-])`, `э`},
+			{`\Aeu`, `ев`}, {`u[eéè]u`, `е`},
+			{`ieu\Z`, `ью`},
+			{`u[eéè]\Z`, `ью`}, {`gi[eéè]\Z`, `ж`},
+			{`nc[eéè]\Z`, `нс`},
+			{`g\Z`, `ж`}, {`z\Z`, `ц`}, {`y\Z`, `и`},
+			{`tion(?=\w?\Z)`, `шн`},
+			{`g(?=[ieyéè])`, `ж`},
+			{`ai(?=[^euioayéèàù-]{2})`, `е`},
+			{`l[eéè]`, `ле`}, {`l[uù]`, `лю`},
+			{`(?<=[euioayéèàù])x{?=[euioayéèàù]\w+}`, `кз`},
+			{`(?<=[euioayéèàù])s(?=[euioayéèàù])`, `з`},
+			{`(?<=\w{3})[e]\Z`, ``},
+			{`(?<=[rdgkzb])h(?!\Z)`, ``},
+
+			{`ph`, `ф`}, {`qu`, `кв`}, {`sc`, `ск`}, {`cs`, `кс`}, {`th`, `т`},
+			{`oi`, `уа`}, {`ou`, `у`}, {`ay`, `ей`}, {`ie`, `ье{`},
+
+			{`b`, `б`}, {`c`, `к`}, {`d`, `д`}, {`f`, `ф`}, {`g`, `г`},
+			{`h`, `х`}, {`j`, `ж`}, {`k`, `к`}, {`l`, `л`}, {`m`, `м`},
+			{`n`, `н`}, {`p`, `п`}, {`q`, `к`}, {`r`, `р`}, {`s`, `с`},
+			{`t`, `т`}, {`v`, `в`}, {`w`, `в`}, {`x`, `кс`}, {`z`, `з`},
+			{`a`, `а`}, {`e`, `е`}, {`i`, `и`}, {`o`, `о`}, {`u`, `у`},
+			{`y`, `и`}, {`é`, `е`}, {`è`, `е`}, {`à`, `а`}, {`ù`, `у`},
+			{`ê`, `е`}, {`â`, `а`}, {`ô`, `о`}, {`î`, `и`}, {`û`, `у`},
+			{`ë`, `е`}, {`ï`, `и`}, {`ü`, `у`}, {`ÿ`, `и`}, {`ç`, `с`},
+		},
+
+		///ITALY
+
+		{
+			{`cch`, `чч`}, {`zz`, `цц`}, {`lum`, `люм`},
+
+			{`\Az`, `з`},
+			{`(?<!l)l(?![leuioay-])`, `ль`}, {`l\Z`, `ль`},
+			{`(?<![euioay-])z`, `ц`},
+			{`cc(?=[ei])`, `чч`}, {`(?<!s)c(?=[ei])`, `ч`},
+			{`gg?(?=[ei])`, `дж`},
+			{`(?<![euioay-])eu(?![euioay-])`, `ью`},
+			{`\Ae(?![euioay-])`, `э`},
+			{`(?<=[euioa])i(?=\w[euioa])`, `й`},
+			{`ue\Z`, `ью`},
+
+			{`cc`, `цц`}, {`ch`, `к`}, {`qu`, `кв`}, {`sh`, `ш`}, {`ts`, `ц`},
+
+			{`b`, `б`}, {`c`, `к`}, {`d`, `д`}, {`f`, `ф`},
+			{`g`, `г`}, {`h`, `х`}, {`j`, `ж`}, {`k`, `к`},
+			{`l`, `л`}, {`m`, `м`}, {`n`, `н`}, {`p`, `п`},
+			{`q`, `к`}, {`r`, `р`}, {`s`, `с`}, {`t`, `т`},
+			{`v`, `в`}, {`w`, `в`}, {`x`, `кс`}, {`z`, `з`},
+			{`a+`, `а`}, {`e+`, `е`}, {`i+`, `и`}, {`o+`, `о`},
+			{`u+`, `у`}, {`y+`, `и`},
+		},
+
+		//GERMAN
+		/*
+			{
+
+				{`tsch`, `ч`}, {`sch`, `ш`}, {`chs`, `хс`}, {`ss`, `сс`},
+
+				{`s(?=[euioay])`, `з`},
+				{`(?<![eiiouay])z(?![euioay])`, `ц`},
+				{`(?<=[euoiay])v(?=[euioay])`, `в`},
+				{`(?<![euioay])ä`, `е`},
+				{`\Ae(?![euioay-])`, `э`},
+				{`(?<!\Ai)st`, `шт`},
+				{`t?z\Z`, `ц`}, {`\Ach`, `к`},
+
+				{`ch`, `х`}, {`tz`, `ц`}, {`sp`, `шп`},
+				{`ck`, `к`}, {`ph`, `ф`}, {`sh`, `ш`},
+				{`eh`, `е`}, {`je`, `е`}, {`ju`, `ю`},
+				{`ja`, `я`}, {`qu`, `кв`}, {`ei`, `ей`},
+				{`ie`, `и`}, {`eu`, `ой`},
+			},
+		*/
+		//LATIN
+		{
+
+			{`\Aa\Z`, `эй`}, {`\Ab\Z`, `би`}, {`\Ac\Z`, `си`}, {`\Ad\Z`, `ди`}, {`\Af\Z`, `эф`}, {`\Ae\Z`, `и`}, {`\Ao\Z`, `оу`}, {`\Ac\Z`, `си`}, {`\Ax\Z`, `экс`}, {`\Aу\Z`, `уай`},
+			{`\Ag\Z`, `джи`}, {`\Ah\Z`, `эйч`}, {`\Aj\Z`, `джей`}, {`\Ak\Z`, `кей`},
+			{`\Al\Z`, `эль`}, {`\Am\Z`, `эм`}, {`\An\Z`, `эн`}, {`\Ap\Z`, `пи`},
+			{`\Aq\Z`, `кью`}, {`\Ar\Z`, `эр`}, {`\As\Z`, `эс`}, {`\At\Z`, `ти`},
+			{`\Av\Z`, `ви`}, {`\Aw\Z`, `даблю`}, {`\Ax\Z`, `икс`}, {`\Az\Z`, `зед`},
+			{`\Aa\Z`, `эй`}, {`\Ao\Z`, `оу`}, {`\Ai\Z`, `ай`}, {`\Au\Z`, `ю`},
+			{`\Ae\Z`, `и`}, {`\Ay\Z`, `вай`},
+
+			{`sch`, `ш`}, {`ya`, `я`}, {`\Aand\Z`, `энд`},
+
+			{`(?<=[euioay])s(?=[euioay])`, `з`},
+			{`l(?![euioaylk])`, `ль`},
+			{`\Ay(?=[euioa])`, `й`},
+			{`\Ae(?![euioay-])`, `э`},
+			{`(?<=[euioa])y\Z`, `й`},
+			{`(?<![euioa])y\Z`, `и`},
+			{`(?<=\w{3})e\Z{`, ``},
+
+			{`qu`, `кв`}, {`ch`, `ч`}, {`sh`, `ш`}, {`ck`, `к`}, {`th`, `т`},
+			{`ju`, `ю`}, {`ja`, `я`}, {`je`, `е`}, {`jo`, `е`},
+			{`ph`, `ф`}, {`sc`, `ск`}, {`you`, `ю`},
+
+			{`b`, `б`}, {`c`, `к`}, {`d`, `д`}, {`f`, `ф`}, {`g`, `г`}, {`h`, `х`},
+			{`j`, `й`}, {`k`, `к`}, {`l`, `л`}, {`m`, `м`}, {`n`, `н`}, {`p`, `п`},
+			{`q`, `к`}, {`r`, `р`}, {`s`, `с`}, {`t`, `т`}, {`v`, `в`}, {`w`, `в`},
+			{`x`, `кс`}, {`z`, `з`}, {`a+`, `а`}, {`e+`, `е`}, {`i+`, `и`},
+			{`u+`, `у`}, {`o+`, `о`}, {`y+`, `и`},
+		},
+		//JAPANESE
+
+		{
+
+			{`sh`, `ш`}, {`ts`, `ц`}, {`ya`, `я`}, {`yo`, `е`}, {`yu`, `ю`},
+			{`aa`, `а`}, {`ee`, `е`}, {`uu`, `у`}, {`ii`, `и`}, {`oo`, `о`},
+
+			{`b`, `б`}, {`d`, `д`}, {`f`, `ф`}, {`g`, `г`},
+			{`h`, `х`}, {`j`, `дж`}, {`k`, `к`}, {`l`, `л`},
+			{`m`, `м`}, {`n`, `н`}, {`p`, `п`}, {`r`, `р`},
+			{`s`, `с`}, {`t`, `т`}, {`w`, `в`}, {`z`, `з`},
+			{`a`, `а`}, {`e`, `е`}, {`i`, `и`}, {`u`, `у`},
+
+			{"e", "е"}, {"o", "о"}, {"b", "б"}, {"c", "к"}, {"d", "д"}, {"f", "ф"}, {"g", "г"}, {"h", "х"}, {"j", "й"}, {"k", "к"}, {"l", "л"}, {"m", "м"}, {"n", "н"}, {"p", "п"}, {"q", "к"}, {"r", "р"}, {"s", "с"}, {"t", "т"}, {"v", "в"}, {"w", "в"}, {"x", "кс"}, {"z", "з"}, {"a", "а"}, {"e", "е"}, {"i", "и"}, {"u", "у"}, {"o", "о"}, {"y", "и"}, {"ß", "сс"}, {"ö", "о"}, {"ä", "а"}, {"ü", "у"}, {"é", "е"}, {"è", "е"}, {"à", "а"}, {"ù", "у"}, {"ê", "е"}, {"â", "а"}, {"ô", "о"}, {"î", "и"}, {"û", "у"}, {"ë", "е"}, {"ï", "и"}, {"ÿ", "и"}, {"ç", "с"},
+		},
+
+		/////////////////////////
+
+	}
+
+	for _, oneLangRules := range multilangRules {
+		newWordMultilang := text
+
+		for _, twoRules := range oneLangRules {
+			re := regexp.MustCompile(twoRules[0], 0)
+			newWordMultilang, _ = re.ReplaceAllString(newWordMultilang, twoRules[1], 0)
+
+		}
+
+		result = append(result, newWordMultilang)
+	}
+
+	//////////////////////////
 	if len([]rune(text)) <= 4 {
 		abbrRules := [][]string{
 			{`b`, `би`}, {`c`, `си`}, {`d`, `ди`}, {`f`, `эф`},
 			{`g`, `джи`}, {`h`, `эйч`}, {`j`, `джей`}, {`k`, `кей`},
 			{`l`, `эль`}, {`m`, `эм`}, {`n`, `эн`}, {`p`, `пи`},
 			{`q`, `кью`}, {`r`, `эр`}, {`s`, `эс`}, {`t`, `ти`},
-			{`v`, `ви`}, {`w`, `дабл ю`}, {`x`, `икс`}, {`z`, `зед`},
+			{`v`, `ви`}, {`w`, `даблю`}, {`x`, `икс`}, {`z`, `зед`},
 			{`a`, `эй`}, {`o`, `оу`}, {`i`, `ай`}, {`u`, `ю`},
 			{`e`, `и`}, {`y`, `вай`},
 		}
@@ -167,21 +320,28 @@ func (obj *Transpiler) Transpile(text string) []string {
 func main() {
 
 	trans := NewTranspiler()
-	fmt.Println(trans.Transpile("apple"))
-	fmt.Println(trans.Transpile("makita"))
-	fmt.Println(trans.Transpile("bosch"))
-	fmt.Println(trans.Transpile("xiaomi"))
-	fmt.Println(trans.Transpile("bergauf"))
-	fmt.Println(trans.Transpile("knauf"))
+	fmt.Println("apple ->", trans.Transpile("apple"))
+	fmt.Println("makita ->", trans.Transpile("makita"))
+	fmt.Println("bosch ->", trans.Transpile("bosch"))
+	fmt.Println("xiaomi ->", trans.Transpile("xiaomi"))
+	fmt.Println("bergauf ->", trans.Transpile("bergauf"))
+	fmt.Println("knauf ->", trans.Transpile("knauf"))
 
-	fmt.Println(trans.Transpile("zelda"))
-	fmt.Println(trans.Transpile("argotech"))
-	fmt.Println(trans.Transpile("rossinka"))
+	fmt.Println("zelda ->", trans.Transpile("zelda"))
+	fmt.Println("argotech ->", trans.Transpile("argotech"))
+	fmt.Println("rossinka ->", trans.Transpile("rossinka"))
 
-	fmt.Println(trans.Transpile("trabadath"))
-	fmt.Println(trans.Transpile("obs"))
-	fmt.Println(trans.Transpile("chs"))
-	fmt.Println(trans.Transpile("fbi"))
+	fmt.Println("trabadath ->", trans.Transpile("trabadath"))
+	fmt.Println("obs ->", trans.Transpile("obs"))
+	fmt.Println("chs ->", trans.Transpile("chs"))
+	fmt.Println("fbi ->", trans.Transpile("fbi"))
 
-	fmt.Println(trans.Transpile("tytan"))
+	fmt.Println("tytan ->", trans.Transpile("tytan"))
+	fmt.Println("qwerty ->", trans.Transpile("qwerty"))
+	fmt.Println("microsoft ->", trans.Transpile("microsoft"))
+	fmt.Println("huawey ->", trans.Transpile("huawey"))
+	fmt.Println("bmw ->", trans.Transpile("bmw"))
+
+	fmt.Println("wolfenstein ->", trans.Transpile("wolfenstein"))
+
 }
